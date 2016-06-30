3d8e6a0173963535bdd72e51e745a7ed120161e7
王贵源 <kerbores@gmail.com>
2016-07-01 07:02:10 +0800

    资源维度丰满

--- monitor/monitor-domain/src/main/java/club/zhcs/monitor/domain/resource/Resource.java
+++ monitor/monitor-domain/src/main/java/club/zhcs/monitor/domain/resource/Resource.java
@@ -36,6 +36,44 @@ public class Resource extends Entity {
 	@Comment("资源主类型")
 	private OwnerType ownerType = OwnerType.PRIVATE;
 
+	@Column("r_test_period")
+	@Comment("监测周期")
+	private TESTINGPERIOD testingperiod;
+
+	@Column("r_task_cron")
+	@Comment("任务Cron表达式")
+	private String taskCron;
+
+	/**
+	 * @return the testingperiod
+	 */
+	public TESTINGPERIOD getTestingperiod() {
+		return testingperiod;
+	}
+
+	/**
+	 * @param testingperiod
+	 *            the testingperiod to set
+	 */
+	public void setTestingperiod(TESTINGPERIOD testingperiod) {
+		this.testingperiod = testingperiod;
+	}
+
+	/**
+	 * @return the taskCron
+	 */
+	public String getTaskCron() {
+		return taskCron;
+	}
+
+	/**
+	 * @param taskCron
+	 *            the taskCron to set
+	 */
+	public void setTaskCron(String taskCron) {
+		this.taskCron = taskCron;
+	}
+
 	/**
 	 * @return the ownerId
 	 */
@@ -162,6 +200,66 @@ public class Resource extends Entity {
 		private OwnerType(String name) {
 			this.name = name;
 		}
+
+	}
+
+	/**
+	 * 
+	 * @author Kerbores(kerbores@gmail.com)
+	 *
+	 * @project monitor-domain
+	 *
+	 * @file Resource.java
+	 *
+	 * @description 监测周期
+	 *
+	 * @time 2016年7月1日 上午6:48:05
+	 *
+	 */
+	public static enum TESTINGPERIOD {
+		SECOND("每秒", "*/1 * * * * *"), FIVE("每5秒", "*/5 * * * * *"), TEN("每10秒", "*/10 * * * * *"), THIRTY("每30秒", "*/30 * * * * *"), Other("自定义", "");
+
+		/**
+		 * @param name
+		 * @param cron
+		 */
+		private TESTINGPERIOD(String name, String cron) {
+			this.name = name;
+			this.cron = cron;
+		}
+
+		/**
+		 * @return the name
+		 */
+		public String getName() {
+			return name;
+		}
+
+		/**
+		 * @param name
+		 *            the name to set
+		 */
+		public void setName(String name) {
+			this.name = name;
+		}
+
+		/**
+		 * @return the cron
+		 */
+		public String getCron() {
+			return cron;
+		}
+
+		/**
+		 * @param cron
+		 *            the cron to set
+		 */
+		public void setCron(String cron) {
+			this.cron = cron;
+		}
+
+		private String name;
+		private String cron;
 
 	}
 }
