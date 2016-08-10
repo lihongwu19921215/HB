05e44923d8069554469fa6b470e503033de9909d
王贵源 <kerbores@gmail.com>
2016-08-10 17:35:30 +0800

    sql done

--- monitor/monitor-service/src/main/java/club/zhcs/monitor/service/record/FtpServerMonitroRecordService.java
+++ monitor/monitor-service/src/main/java/club/zhcs/monitor/service/record/FtpServerMonitroRecordService.java
@@ -1,11 +1,18 @@
 package club.zhcs.monitor.service.record;
 
-import org.nutz.dao.Sqls;
+import java.sql.Connection;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.List;
+
 import org.nutz.dao.sql.Sql;
-import org.nutz.lang.util.LinkedLongArray;
+import org.nutz.dao.sql.SqlCallback;
+import org.nutz.lang.Lang;
 
 import club.zhcs.monitor.domain.record.FtpServerMonitroRecord;
 import club.zhcs.titans.utils.biz.BaseService;
+
+import com.google.common.collect.Lists;
 
 /**
  * 
@@ -25,27 +32,48 @@ public class FtpServerMonitroRecordService extends BaseService<FtpServerMonitroR
 	 * @param id
 	 * @return
 	 */
-	public long[] getConnectionTimes(int id) {
+	public Long[] getConnectionTimes(int id) {
 		Sql sql = create("get.ftp.monitor.record.duration.by.type");
 		sql.params().set("type", FtpServerMonitroRecord.CheckType.CONNECTION);
 		sql.vars().set("id", id);
-		sql.setCallback(Sqls.callback.longs());
+		sql.setCallback(new SqlCallback() {
+
+			@Override
+			public Object invoke(Connection conn, ResultSet rs, Sql sql) throws SQLException {
+				List<Long> target = Lists.newArrayList();
+				while (rs.next()) {
+					target.add(rs.getLong(1));
+				}
+				return target;
+			}
+		});
 		dao().execute(sql);
-		LinkedLongArray ls = sql.getObject(LinkedLongArray.class);
-		return ls.toArray();
+
+		return Lang.collection2array(sql.getList(Long.class));
 	}
 
 	/**
 	 * @param id
 	 * @return
 	 */
-	public long[] getDownloadTimes(int id) {
+	public Long[] getDownloadTimes(int id) {
 		Sql sql = create("get.ftp.monitor.record.duration.by.type");
 		sql.params().set("type", FtpServerMonitroRecord.CheckType.DOWNLOAD);
 		sql.vars().set("id", id);
-		sql.setCallback(Sqls.callback.longs());
+		sql.setCallback(new SqlCallback() {
+
+			@Override
+			public Object invoke(Connection conn, ResultSet rs, Sql sql) throws SQLException {
+				List<Long> target = Lists.newArrayList();
+				while (rs.next()) {
+					target.add(rs.getLong(1));
+				}
+				return target;
+			}
+		});
 		dao().execute(sql);
-		return sql.getObject(LinkedLongArray.class).toArray();
+
+		return Lang.collection2array(sql.getList(Long.class));
 	}
 
 }
